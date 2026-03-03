a7e7b21e162ed44ef9c95299bab5ef34d4e71311
simplysamiul <simplysamiul@gmail.com>
2026-03-03 12:48:31 +0600

    make update doctor business logic

--- src/app/module/doctor/doctor.interface.ts
+++ src/app/module/doctor/doctor.interface.ts
@@ -1,8 +1,25 @@
+import { Gender } from "../../../generated/prisma/enums";
 
-export interface IupdateDoctor {
-    name?: string;
-    profilephoto?: string;
-    contactNumber?: string;
-    address?: string;
-    experience?: number;
+
+export interface IUpdateDoctorSpecialtyPayload {
+    specialtyId: string;
+    shouldDelete?: boolean;
+
+}
+
+export interface IUpdateDoctorPayload {
+    doctor?: {
+        name?: string;
+        profilePhoto?: string;
+        contactNumber?: string;
+        address?: string;
+        experience?: number
+        registrationNumber?: string;
+        gender?: Gender;
+        appointmentFee?: number;
+        qualification?: string;
+        currentWorkingPlace?: string;
+        designation?: string;
+    },
+    specialties?: IUpdateDoctorSpecialtyPayload[];
 }
--- src/app/module/doctor/doctor.service.ts
+++ src/app/module/doctor/doctor.service.ts
@@ -1,13 +1,16 @@
+import status from "http-status";
+import AppError from "../../errorHelpers/AppError";
 import { prisma } from "../../lib/prisma";
+import { IUpdateDoctorPayload } from "./doctor.interface";
 
 
 const getAllDoctors = async () => {
     const doctors = await prisma.doctor.findMany({
         include: {
             user: true,
-            specialities: {
+            specialties: {
                 include: {
-                    speciality: true
+                    specialty: true
                 }
             }
         }
@@ -16,15 +19,15 @@ const getAllDoctors = async () => {
     return doctors;
 };
 
-const getDoctorById = async(id: string) => {
+const getDoctorById = async (id: string) => {
     const doctro = await prisma.doctor.findUnique({
-        where : {
+        where: {
             id
-        },include: {
+        }, include: {
             user: true,
-            specialities: {
+            specialties: {
                 include: {
-                    speciality: true
+                    specialty: true
                 }
             }
         }
@@ -35,10 +38,69 @@ const getDoctorById = async(id: string) => {
 };
 
 
+const updateDoctor = async (id: string, payload: IUpdateDoctorPayload) => {
+    const doctorExist = await prisma.doctor.findUnique({
+        where: {
+            id
+        }
+    });
+
+    if (!doctorExist) {
+        throw new AppError(status.NOT_FOUND, "Doctor not found")
+    };
+
+    const { doctor: doctorData, specialties } = payload;
+
+    await prisma.$transaction(async (tx) => {
+        if (doctorData) {
+            await tx.doctor.update({
+                where: { id },
+                data: { ...doctorData }
+            })
+        }
+
+        if (specialties && specialties.length > 0) {
+            for (const specialty of specialties) {
+                const { specialtyId, shouldDelete } = specialty;
+                if (shouldDelete) {
+                    await tx.doctorSpecialty.delete({
+                        where: {
+                            doctorId_specialtyId: {
+                                doctorId: id,
+                                specialtyId,
+                            }
+                        }
+                    })
+                } else {
+                    await tx.doctorSpecialty.upsert({
+                        where: {
+                            doctorId_specialtyId: {
+                                doctorId: id,
+                                specialtyId
+                            }
+                        },
+                        create: {
+                            doctorId: id,
+                            specialtyId
+                        },
+                        update: {}
+                    })
+                }
+            }
+        }
+    })
+
+    const doctor = await getDoctorById(id);
+
+    return doctor;
+
+}
+
 
 
 
 export const doctorService = {
     getAllDoctors,
-    getDoctorById
+    getDoctorById,
+    updateDoctor
 };
